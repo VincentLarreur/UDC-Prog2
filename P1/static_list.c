d28309837cb4c9dd897eecb54ac1d34847ab388a
Le Rhun Maugan <maugan.lerhun@etudiant.univ-rennes1.fr>
2020-03-25 18:00:38 +0100

    static list maugan

--- P1/static_list.c
+++ P1/static_list.c
@@ -11,102 +11,115 @@
 
 
 
-tList createEmptyList(){
-	tList * l=malloc(sizeof(tList))
-	for (int i=0;i<25;i++){
-		l+i=NULL;
+void createEmptyList (tList* l){
+	l=malloc(sizeof(tList));
+	for (int i=0;i<MAX;i++){
+	    tItemL * p=&(l->tableau[i]);
+        p=LNULL;
 	}
-	return *l;
 }
-// returns tList
-// Creates an empty list
-// PostCD: The list is initialised and has no elements
+
 
 bool isEmptyList (tList l){
-	for(int i=0;i<25;i++){
-		if(&l[i]!=NULL){
+	for(int i=0;i<MAX;i++){
+		if(&(l.tableau[i])!=LNULL){
 			return false;
 		}
 	}
 	return true;
 }
-// Determines whether the list is empty or not. 
+
 
 tPosL first (tList l) {
-	return 0;
+    if(isEmptyList(l)) return LNULL;
+    else return 0;
 }
-// Returns the position of the first element of the list. PreCD: The list is not empty
+
+tPosL last(tList l){
+    if(isEmptyList(l)) return LNULL;
+    else{
+        int i;
+        for(i=0;i<MAX;i++){
+            if(&(l.tableau[i])==LNULL) return i-1;
+        }
+    }
+}
+
 
 tPosL next (tPosL pos, tList l){
-	if(l[pos+1]==NULL){
-		return NULL;
+	if(&(l.tableau[pos+1])==LNULL){
+		return LNULL;
 	}
 	else {
 		return pos+1;
 	}
 }
-//→ tPosL  Returns the position following that one we indicate (or LNULL if the specified position has no next element). PreCD: The indicated position is a valid position in the list. 
+
 
 tPosL previous (tPosL pos, tList l){
-	if(l[pos-1]==NULL){
-		return NULL;
+	if(&(l.tableau[pos-1])==LNULL){
+		return LNULL;
 	}
 	else{
 		return pos-1;
 	}
 }   
-//Returns the position preceding the one we indicate (or LNULL if the specified position has no previous element). PreCD: The indicated position is a valid position in the list. 
- 
-tList insertItem (tItemL item, tPosL pos, tList l){
-	if (pos==LNULL){
-		int i;
-		for(i=0;i<25;i++){
-			if(l[i]==NULL){
-				l[i]=item;
-				break;
-			}
-		}	
-	}
-	else if (l[24]==NULL){
-		int i;
-		for(i=24;i>pos;i--){
-			l[i]=l[i-1];
-		}
-		l[pos]=item;
-	}
-	return l;
+
+bool isFullList (tList l){
+    int i;
+    for(i=0;i<MAX;i++){
+        if (&(l.tableau[i])==LNULL) return false;
+    }
+    return true;
 }
-//→ tList, bool  Inserts an element containing the provided data item in the list. If the specified position is LNULL, then the element is added at the end of the list; otherwise, it will be placed right before the element currently holding that position. It the element could be inserted, the value true is returned, false otherwise.  PreCD: The specified position is a valid position in the list or a LNULL position. PostCD: The positions of the elements in the list subsequent to the inserted one may have changed their values. 
- 
-tList deleteAtPosition (tPosL pos, tList l){
+bool insertItem (tItemL item, tPosL pos, tList * l){
+    if(pos > MAX-1) return false;
+    if(isFullList(*l)) return false;
+    if (&(l->tableau[MAX-1])==LNULL){
+        if (pos==LNULL){
+            int i;
+            for(i=0;i<25;i++){
+                if(&(l->tableau[i])==NULL){
+                    l->tableau[i]=item;
+                    return true;
+                }
+            }
+        }
+        int i;
+        for(i=MAX-1;i>pos;i--){
+            l[i]=l[i-1];
+        }
+        l->tableau[pos]=item;
+        return true;
+    }
+}
+
+void deleteAtPosition (tPosL pos, tList * l){
 	int i;
-	free(l[pos]);
-	for(i=pos;i<24;i++){
+	free(&(l->tableau[pos]));
+	for(i=pos;i<MAX-1;i++){
 		l[i]=l[i+1];
 	}
+}
 
-	return l;
-}
-//→ tList  Deletes the element at the given position from the list. 
-//PreCD: The indicated position is a valid position in the list. PostCD: The deleted position and the positions of the elements of the list subsequent to it may have changed their values. 
- 
+
 tItemL getItem (tPosL pos, tList l){
-	return l[pos];
+	return l.tableau[pos];
 }
-// → tItemL  Retrieves the content of the element at the position we indicate. PreCD: The indicated position is a valid position in the list. 
- 
-tList updateVotes (tNumVotes v, tPosL pos, tList l){
-	l[pos].numVotes=v;
-	return l;
+
+void updateVotes (tNumVotes v, tPosL pos, tList * l){
+	l->tableau[pos].numVotes=v;
 }
-// → tList  Modifies the number of votes of the element at the position we indicate. PreCD: The indicated position is a valid position in the list. PostCD: The order of the elements in the list has not been modified. 
- 
-tPosL findItem (tPartyName party, tList l){
-	for(int i=0;i<25;i++){
-		if (l[i].partyName==party){
+
+tPosL findItem (tPartyName n, tList l){
+	for(int i=0;i<MAX;i++){
+		if (l.tableau[i].partyName==n){
 			return i;
 		}
 	}
-	return NULL;
+	return LNULL;
 }
-// → tPosL  Returns the position of the first element in the list whose party name is the one indicated (or LNULL if there is no such item).
+
+void deleteList(tList* l){
+    free(l);
+}
--- P1/static_list.h
+++ P1/static_list.h
@@ -12,46 +12,45 @@
 
 #include "types.h"
 #include <stdbool.h>
-
+#include <stdlib.h>
+/*
 typedef int tPosL;
-const tPosL LNULL=NULL;
+
 typedef struct {
 	tItemL tableau[25];
 	tPosL pos;
 } tList;
+*/
+#define LNULL NULL
+#define MAX 25
+
+typedef int tPosL;
+
+typedef struct tList {
+    tItemL tableau[MAX];
+    tPosL pos;
+} tList;
 
 
-tList createEmptyList();
-// returns tList
-// Creates an empty list
-// PostCD: The list is initialised and has no elements
+void createEmptyList (tList* l);
+bool isEmptyList(tList l);
+tPosL first(tList l);
+tPosL last(tList l);
+tPosL next(tPosL pos, tList l);
+tPosL previous(tPosL pos, tList l);
+bool insertItem(tItemL item, tPosL pos, tList* l);
+void deleteAtPosition(tPosL pos, tList* l);
+tItemL getItem(tPosL pos, tList l);
+void updateVotes(tNumVotes vote, tPosL pos, tList* l);
+tPosL findItem(tPartyName n, tList l);
+void deleteList(tList* l);
 
-bool isEmptyList (tList l);
-// Determines whether the list is empty or not. 
+bool copyList(tList l1, tList* l2);
 
-tPosL first (tList l);
-// Returns the position of the first element of the list. PreCD: The list is not empty
 
-tPosL next (tPosL pos, tList l); 
-//→ tPosL  Returns the position following that one we indicate (or LNULL if the specified position has no next element). PreCD: The indicated position is a valid position in the list. 
 
-tPosL previous (tPosL pos, tList l);   
-//Returns the position preceding the one we indicate (or LNULL if the specified position has no previous element). PreCD: The indicated position is a valid position in the list. 
- 
-tList insertItem (tItemL item, tPosL pos, tList l); 
-//→ tList, bool  Inserts an element containing the provided data item in the list. If the specified position is LNULL, then the element is added at the end of the list; otherwise, it will be placed right before the element currently holding that position. It the element could be inserted, the value true is returned, false otherwise.  PreCD: The specified position is a valid position in the list or a LNULL position. PostCD: The positions of the elements in the list subsequent to the inserted one may have changed their values. 
- 
-tList deleteAtPosition (tPosL pos, tList l); 
-//→ tList  Deletes the element at the given position from the list. 
-//PreCD: The indicated position is a valid position in the list. PostCD: The deleted position and the positions of the elements of the list subsequent to it may have changed their values. 
- 
-tItemL getItem (tPosL pos, tList l);
-// → tItemL  Retrieves the content of the element at the position we indicate. PreCD: The indicated position is a valid position in the list. 
- 
-tList updateVotes (tNumVotes v, tPosL pos, tList l);
-// → tList  Modifies the number of votes of the element at the position we indicate. PreCD: The indicated position is a valid position in the list. PostCD: The order of the elements in the list has not been modified. 
- 
-tPosL findItem (tPartyName party, tList l);
-// → tPosL  Returns the position of the first element in the list whose party name is the one indicated (or LNULL if there is no such item).
+
+
+
 
 #endif
